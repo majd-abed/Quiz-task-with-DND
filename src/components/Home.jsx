fb174313b270da36631088dd567ee5f0f484a502
Majd Abed <majdcx@hotmail.com>
2023-03-25 22:34:16 +0300

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,13 +1,13 @@
 export default function Home() {
   return (
-    <div className="h-[95vh]">
+    <>
       <p className='text-gray-700 text-3xl mb-16 font-bold '>Dashboard</p>
-      <div className='grid lg:grid-cols-3 gap-5 mb-16'>
+      <div className='grid lg:grid-cols-3 gap-5 mb-16 smx:mb-6'>
         <div className='rounded bg-white h-40 shadow-sm'></div>
         <div className='rounded bg-white h-40 shadow-sm'></div>
         <div className='rounded bg-white h-40 shadow-sm'></div>
       </div>
-      <div className='grid col-1 bg-white h-56 shadow-sm'></div>
-    </div>
+      <div className='grid col-1 rounded bg-white h-56 shadow-sm mb-4'></div>
+    </>
   );
 }
